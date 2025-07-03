f2d5a1612d7365acba56c3f6d63299874dc1a1fc
yinfengjiujian <duanmeilin911@163.com>
2025-07-03 21:41:07 +0800

    分解微服务

---
+++ reactor-service/src/test/java/com/duanml/reactorservice/ReactorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duanml.reactorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
